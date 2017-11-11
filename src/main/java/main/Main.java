46ede74dfe2f379ccda7a010395310b4ff7deb5f
Elmeri Lehikoinen <elmeri.lehikoinen@gmail.com>
2017-11-11 18:52:03 +0200

    codacyCommit1

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,11 @@
 package main;
 
-import ohtu.ohtuvarasto.Varasto;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("testi");
+        
     }
 }
